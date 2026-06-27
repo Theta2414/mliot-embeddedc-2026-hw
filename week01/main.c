cf809ce733601067cb55b979bd0be20db42809a2
Theta <169332632+Theta2414@users.noreply.github.com>
2026-06-27 23:40:27 +0700

    week01: hoan thanh bai tap buoi 1

--- week01/main.c
+++ week01/main.c
@@ -7,10 +7,8 @@
 
 void parse_config(const uint8_t *config_packet, int16_t *high_threshold) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
-
-
-
+    if (config_packet == NULL || high_threshold == NULL) return;
+    *high_threshold = (config_packet[1] << 8) | config_packet[0];
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
 
@@ -18,10 +16,8 @@ void parse_config(const uint8_t *config_packet, int16_t *high_threshold) {
 
 int16_t read_temperature_reg(void *hw_sensor_reg) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
-
-
-
+    if (hw_sensor_reg == NULL) return;
+    return *(volatile int16_t*)hw_sensor_reg;
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
 
@@ -29,10 +25,17 @@ int16_t read_temperature_reg(void *hw_sensor_reg) {
 
 void control_output(uint8_t *control_reg, uint8_t fan_enable, uint8_t alarm_enable) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
-
-
-
+    if (control_reg == NULL) return;
+    if (fan_enable) {
+        *control_reg = *control_reg | (1 << 0);
+    } else {
+        *control_reg = *control_reg & ~(1 << 0);
+    }
+    if (alarm_enable) {
+        *control_reg = *control_reg | (1 << 1);
+    } else {
+        *control_reg = *control_reg & ~(1 << 1);
+    }
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
 
